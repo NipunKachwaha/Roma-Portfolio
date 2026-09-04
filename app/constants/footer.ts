0b99a83286e851788e0628128d8709abf7e8687b
Nipun <nipunrajput077@gmail.com>
2026-09-04 13:26:32 +0530

    time

---
+++ app/constants/footer.ts
@@ -0,0 +1,34 @@
+import { FooterLink } from "../types";
+
+export const FOOTER_LINKS: FooterLink[] = [
+  {
+    name: 'LinkedIn',
+    hoverText: 'Connect with me',
+    icon: 'icons/linkedin.svg',
+    url: 'https://www.linkedin.com/in/nipun-/',
+  },
+  {
+    name: 'GitHub',
+    hoverText: 'Open Sourcing',
+    icon: 'icons/github.svg',
+    url: 'https://github.com/NipunKachwaha',
+  },
+  {
+    name: 'X',
+    hoverText: 'Follow on X',
+    icon: 'icons/x2.svg',
+    url: '',
+  },
+  {
+    name: 'Instagram',
+    hoverText: '@thakurxnipun',
+    icon: 'icons/instagram.svg',
+    url: 'https://www.instagram.com/thakurxnipun/',
+  },
+  {
+    name: 'Resume',
+    hoverText: 'Download',
+    icon: 'icons/file.svg',
+    url: './Mohit Virli - Resume.pdf',
+  }
+];
